1e8d6e8f5c26c7fd6b0ad0a4a60c25a979928b81
waseem423 <w3290586@gmail.com>
2025-11-05 18:00:17 +0300

    Second Commit

---
+++ Bank System Bussiness Layer/clsForTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_System_Bussiness_Layer
+{
+    internal class clsForTest
+    {
+
+        public string PrintHellowWorld()
+        {
+            return "Hello World";
+        }
+
+    }
+}
