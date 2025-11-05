6970a737ee04a0d4710a37ab718eaa0bd076b3c0
waseem423 <w3290586@gmail.com>
2025-11-05 18:07:37 +0300

    second Commit

---
+++ Bank System Data Access Layer/clsForTestDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_System_Data_Access_Layer
+{
+    internal class clsForTestDataAccess
+    {
+        public string GetstringHelloWorldFromDataBase()
+        {
+            return "Hello World From Data Base";
+        }
+    }
+}
